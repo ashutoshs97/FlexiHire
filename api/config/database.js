83f9bec6cac04982bc39233d0334a46f38263e91
ashutoshs97 <87438951+ashutoshs97@users.noreply.github.com>
2025-04-09 00:42:55 +0530

    v5_hosting

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const MongoConnect = () => {
-  mongoose.connect(process.env.MONGODB);
+  // mongoose.connect(process.env.MONGODB);
+  mongoose.connect("mongodb+srv://ashutoshs97:admin123@cluster0.xhipuoo.mongodb.net/FlexDB?retryWrites=true&w=majority&appName=Cluster0");
   const db = mongoose.connection;
   db.on("error", (err) => {
     console.log("Database Connection Error: " + err.message);
