f12a8a622d36e24b65bad2a73c0c0d475f9c3a99
ashutoshs97 <87438951+ashutoshs97@users.noreply.github.com>
2025-04-09 01:11:43 +0530

    v5_hosting_fixes

--- client/src/Redux/myAxios.js
+++ client/src/Redux/myAxios.js
@@ -1,5 +1,12 @@
+// client/src/Redux/myAxios.js
+
 import axios from "axios";
 
-export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+// For debugging
+console.log("API Base URL:", process.env.REACT_APP_API_URL);
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
+export default instance;
