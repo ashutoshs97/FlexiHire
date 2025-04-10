7dde43d6d411c7338ad371329bb93cc45319c951
ashutoshs97 <87438951+ashutoshs97@users.noreply.github.com>
2025-04-10 15:52:36 +0530

    Revert "v5_hosting_fixes"
    This reverts commit f12a8a622d36e24b65bad2a73c0c0d475f9c3a99.

--- client/src/Redux/myAxios.js
+++ client/src/Redux/myAxios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL,
 });
